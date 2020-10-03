1add23b7c0bb048e2c78847a72570ec1f28b4d78
VitaliyV <55128761+VitaliyZasada@users.noreply.github.com>
2020-10-03 18:07:28 +0300

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ok=$(wget --server-response http://10.0.11.11:9999/biotestmine/ 2>&1 | awk '/^  HTTP/{print $2}')
+
+if [[ "$ok" != "200" ]]; then
+		echo Something wrong with your server, please check logs
+			exit 1
+	else
+		echo Return code from the server is $ok ! Server is running
+		#docker stop nginx_server 1> /dev/null
+		#docker rm nginx_server 1> /dev/null
+fi
